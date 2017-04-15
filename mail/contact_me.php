f02ad9b6a621cfb58ffc377b48e9431f9aecbd53
Aswin Varghese <ashwin.vargheese@gmail.com>
2017-04-15 11:03:28 +0530

    email to customer added

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,20 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$to = 'trioeventsmanage@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Event Enquiry Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
+
+//Create email to the customer
+$to = $email_address;
+$email_subject = "Event Enquiry Registered !!! ";
+$email_body = "Hi $name\nWe have receieved your mail regarding Event Enquiry. Our team has registered your request and is processing it.\n\nDetails provided are :\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message\n\nThank you,\nTRIO Events & Weddings";
+$headers = "From : noreply@yourdomain.com\n";
+$header .= "Reply-To:'trioeventsmanage@gmail.com'";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
